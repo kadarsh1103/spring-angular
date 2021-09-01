0c397ef195a6a01a0c3b4e2d199def385ce87844
Adarsh Kumar <adarsh.kumar@qordata.com>
2021-09-01 19:40:14 +0500

    few changes

--- src/main/java/com/example/employeemanagerapp/repository/EmployeeRepository.java
+++ src/main/java/com/example/employeemanagerapp/repository/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.example.employeemanagerapp.repository;
 
 import com.example.employeemanagerapp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
